51f2673c6baaaf9cd1961920e2a293a44f17efed
nghia12a1 <nghianvfx10608@funix.edu.vn>
2022-01-15 10:03:26 +0700

    Update NVIC

---
+++ NVIC/src/NVIC.c
@@ -0,0 +1,29 @@
+//#include <stdio.h>
+#include "NVIC.h"
+
+void Clock_Init(void)
+{
+	RCC->CR |= (1 << 0);
+}
+
+void myDelay(uint32_t time)
+{
+	uint32_t i;
+	while(time--)
+	{
+		for(i = 0; i < 1000; i++);
+	}
+}
+
+void GPIO_Init(void)
+{
+	/* PC13 as INPUT pullup mode */
+	RCC->AHB1 |= (1 << 2);
+	PC->PU |= (1 << 26);
+		
+	/* PA5 as OUTPUT Push-Pull mode */
+	RCC->AHB1 |= (1 << 0);
+	PA->MODE |= (1 << 10); 
+	PA->TYPE &= ~(1 << LED_GREEN);
+	PA->SPEED |= (1 << 10);
+} 
